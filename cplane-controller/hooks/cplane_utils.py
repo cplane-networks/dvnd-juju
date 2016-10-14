308e99711fbc1ca6ddbf548f5507300f1acd4979
amarendrameher <amarendra@rebaca.com>
2016-10-14 10:34:08 +0530

    Deleted some log messages

--- cplane-controller/hooks/cplane_utils.py
+++ cplane-controller/hooks/cplane_utils.py
@@ -303,7 +303,7 @@ def start_services(install_type):
         subprocess.check_call(cmd)
         os.chdir(saved_path)
     else:
-        log("REBACA setup not copleted")
+        log("Setup not copleted")
 
 
 def set_config(key, value, config_file):
